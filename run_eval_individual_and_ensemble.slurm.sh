0566335378a0cfdf64f6077ca0e6f0acbd52e2c6
Luphuc2005 <phucga150625@gmail.com>
2026-09-13 07:32:37 +0700

    Fix SLURM config: set cpus-per-task to 16 and add gres for gpu-queue

--- run_eval_individual_and_ensemble.slurm.sh
+++ run_eval_individual_and_ensemble.slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH --account=sokhcn
 #SBATCH --qos=gpu-q
 #SBATCH --gres=gpu:v100:1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
 #SBATCH --output=/home/ptbao/projects/FER2013_MGR_CNN/logs/EVAL_INDIVIDUAL_ENSEMBLE_%j.out
 #SBATCH --error=/home/ptbao/projects/FER2013_MGR_CNN/logs/EVAL_INDIVIDUAL_ENSEMBLE_%j.err
--- run_eval_individual_and_ensemble_cpu.slurm.sh
+++ run_eval_individual_and_ensemble_cpu.slurm.sh
@@ -3,7 +3,8 @@
 #SBATCH --partition=gpu-queue
 #SBATCH --account=sokhcn
 #SBATCH --qos=gpu-q
-#SBATCH --cpus-per-task=32
+#SBATCH --gres=gpu:v100:1
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
 #SBATCH --output=/home/ptbao/projects/FER2013_MGR_CNN/logs/EVAL_IND_ENS_CPU_%j.out
 #SBATCH --error=/home/ptbao/projects/FER2013_MGR_CNN/logs/EVAL_IND_ENS_CPU_%j.err
